ce1e1dc0f26ff39b83f4ba360d17f2ebb5c9832c
chandraprakashsaini480-a11y <chandraprakashsaini480@gmail.com>
2025-12-30 20:25:21 +0530

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.options = {
-    "domain": "3nbf4.com",
-    "zoneId": 10384384
+    "domain": "5gvci.com",
+    "zoneId": 10395547
 }
 self.lary = ""
-importScripts('https://3nbf4.com/act/files/service-worker.min.js?r=sw')
+importScripts('https://5gvci.com/act/files/service-worker.min.js?r=sw')
